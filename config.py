8586eeeb63de532357ed0214559a0c724fcfec1c
Tim Atkins <tne8288@gmail.com>
2025-10-04 15:24:02 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+```python
+class AssistantConfig:
+    DEFAULT_NAME = "Sophia"
+    DEFAULT_GENDER = "female"
+    DEFAULT_PERSONALITY = "friendly_and_casual"  # Default personality configuration
+```
