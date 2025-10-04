b8afe1364910e0465bb0334f876c2bc3024f3174
Tim Atkins <tne8288@gmail.com>
2025-10-04 15:23:28 -0400

    Create magic_eight_ball.py

---
+++ magic_eight_ball.py
@@ -0,0 +1,19 @@
+```python
+import random
+
+responses = [
+    "Yes, definitely!",
+    "Ask again later.",
+    "No way!",
+    "Absolutely!",
+    "I wouldn't count on it.",
+    "Yes!",
+    "My sources say no.",
+    "Very likely.",
+    "Don't hold your breath.",
+    "Cannot predict now.",
+]
+
+def ask_eight_ball():
+    return random.choice(responses)
+```
